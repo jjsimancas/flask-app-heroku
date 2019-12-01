1aae2fceda1d8c189b23748ab7f9e83e8c6ca444
Juan Simancas <jjsimancas@gmail.com>
2019-12-01 19:08:40 -0300

    Fix return inf

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World with flask redeploy!'
+    return 'Hello World with flask redeploy again!'
 
 
 if __name__ == '__main__':
